db60216a1905cc28c2838bfe779bc1c5a8514211
emccar22 <e.mccarthy2285@gmail.com>
2022-01-12 22:36:33 -0500

    add route to update candidates party

--- server.js
+++ server.js
@@ -109,6 +109,36 @@ app.post('/api/candidate', ({ body }, res) => {
     });
 });
 
+// Update a candidates party
+app.put('/api/candidate/:id', (req, res) => {
+    const errors = inputCheck(req.body, 'party_id');
+
+        if (errors) {
+            res.status(400).json({ error: errors });
+            return;
+        }
+        
+    const sql = `UPDATE candidates SET party_id = ?
+                 WHERE id = ?`;
+    const params = [req.body.party_id, req.params.id];
+    db.query(sql, params, (err, result) => {
+        if (err) {
+            res.status(400).json({ error: err.message });
+            // Check if record was found
+        } else if (!result.affectedRows) {
+            res.json({
+                message: 'Candidate not found'
+            });
+        } else {
+            res.json({
+                message: 'success',
+                data: req.body,
+                changes: result.affectedRows
+            });
+        }
+    });
+});
+
 app.get('/api/parties', (req, res) => {
     const sql = `SELECT * FROM parties`;
     db.query(sql, (err, rows) => {
