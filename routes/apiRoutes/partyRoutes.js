7016312524d53d56d2e5ea51aeebce2016ef44aa
emccar22 <e.mccarthy2285@gmail.com>
2022-01-12 23:38:19 -0500

    add voter get routes

--- routes/apiRoutes/partyRoutes.js
+++ routes/apiRoutes/partyRoutes.js
@@ -2,7 +2,6 @@ const express = require('express');
 const router = express.Router();
 const db = require('../../db/connection');
 
-
 router.get('/parties', (req, res) => {
     const sql = `SELECT * FROM parties`;
     db.query(sql, (err, rows) => {
